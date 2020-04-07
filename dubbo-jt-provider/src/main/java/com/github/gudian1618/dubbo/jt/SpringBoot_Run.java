e574b5b74ab32e33dbb0f8de77a6a3ae6a4a37c5
gudian1618 <1761844211@qq.com>
2020-04-07 21:19:57 +0800

    new

--- dubbo-jt-provider/src/main/java/com/github/gudian1618/dubbo/jt/SpringBoot_Run.java
+++ dubbo-jt-provider/src/main/java/com/github/gudian1618/dubbo/jt/SpringBoot_Run.java
@@ -1,11 +1,13 @@
 package com.github.gudian1618.dubbo.jt;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.github.gudian1618.dubbo.jt.mapper")
+@EnableDubbo
 public class SpringBoot_Run {
 
     public static void main(String[] args) {
